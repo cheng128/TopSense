5dc2f40ba66710ef9c828641526fec54b16ab098
cheng128 <chuchuu128@gmail.com>
2022-02-14 14:40:14 +0800

    [modified]change embedding model

--- data_preprocess/cal_def_emb.py
+++ data_preprocess/cal_def_emb.py
@@ -2,20 +2,33 @@ import json
 import pickle
 from tqdm import tqdm
 from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer('all-roberta-large-v1')
+model = SentenceTransformer('sentence-t5-xl')
 
 def main():
     
-    with open('../data/words2defs.json') as f:
-        word2def = json.loads(f.read())
+#     with open('../data/words2defs.json') as f:
+#         word2def = json.loads(f.read())
+    
+#     def2emb = {}
+#     for definitions in tqdm(word2def.values()):
+#         for sense in definitions:
+#             def2emb[sense] = model.encode(sense,
+#                                           convert_to_tensor=True)
+            
+#     with open('../data/definition_emb.pickle', 'wb') as f:
+#         pickle.dump(def2emb, f)
+
+    with open('../data/cambridge.sense.000.jsonl') as f:
+        cambridge = [json.loads(line) for line in f.readlines()]
     
     def2emb = {}
-    for definitions in tqdm(word2def.values()):
-        for sense in definitions:
-            def2emb[sense] = model.encode(sense,
-                                          convert_to_tensor=True)
-            
-    with open('../data/definition_emb.pickle', 'wb') as f:
+    for line in tqdm(cambridge):
+        if line['pos'] == 'noun':
+            sentences = [line['en_def']]
+            def2emb[line['en_def']] = model.encode(sentences,
+                                              convert_to_tensor=True)
+        
+    with open('../data/t5-xl_def_examples_emb.pickle', 'wb') as f:
         pickle.dump(def2emb, f)
     
 if __name__ == '__main__':
