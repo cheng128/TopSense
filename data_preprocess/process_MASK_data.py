07fbe073915b3419041ccdea865577c9418bb037
cheng128 <chuchuu128@gmail.com>
2022-02-14 14:42:56 +0800

    [modified]try to add definition into training data

--- data_preprocess/process_MASK_data.py
+++ data_preprocess/process_MASK_data.py
@@ -7,15 +7,15 @@ from tqdm import tqdm
 from utils import handle_examples
 
 def brt_groups_results(reserve=False):
-    with open('../data/cambridge.sense.000.jsonl') as f:
-        cambridge = json.loads(f.read())
+#     with open('../data/cambridge.sense.000.jsonl') as f:
+#         cambridge = [json.loads(line) for line in f.readlines()]
     
-    id2def = {line['id']: line['en_def'] for line in cambridge}
+#     id2def = {line['id']: line['en_def'] for line in cambridge}
     
-    with open('../data/word_id.topics.examples.json') as f:
+    with open('../data/remap.word_id.topics.examples.json') as f:
         data = json.loads(f.read())
     
-    with open(f'../data/training_data/brt_masked_noun.tsv', 'a') as f:
+    with open(f'../data/training_data/remap_brt_masked_noun_def.tsv', 'a') as f:
         for k, v in tqdm(data.items()):
             if v['pos'] == 'noun':
                 for topic in list(set(v['topics'])):
@@ -29,14 +29,15 @@ def brt_groups_results(reserve=False):
                         if topic_sent and masked_sent:
                             f.write('\t'.join([sent_en, topic_sent, 
                                                masked_sent]) + '\n')
-                    definition = headword + 'is' + is2def[k]
-                    topic_sent, masked_sent = handle_examples(headword, 
-                                                              sent_en, 
-                                                              topic, 
-                                                              reserve)
-                    if topic_sent and masked_sent:
-                        f.write('\t'.join([sent_en, topic_sent, 
-                                           masked_sent]) + '\n')
+#                     definition = headword.capitalize() + ' is ' + id2def[k]
+#                     definition = definition.replace('\n', '')
+#                     topic_sent, masked_sent = handle_examples(headword, 
+#                                                               definition, 
+#                                                               topic, 
+#                                                               reserve)
+#                     if topic_sent and masked_sent:
+#                         f.write('\t'.join([definition, topic_sent, 
+#                                            masked_sent]) + '\n')
 
 def main():
     parser = argparse.ArgumentParser()
