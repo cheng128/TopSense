32d3117f0b411e436ab0c32f043922545cb79f82
cheng128 <chuchuu128@gmail.com>
2022-02-14 15:32:38 +0800

    [modified]300 evaluation sentences

--- evaluation/evaluation_sentences.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-nlp = spacy.load("en_core_web_sm")
-model = pipeline('fill-mask',
-                 model=f"../model/wiki_similarity/all_10epochs",
-                 tokenizer="../topic_tokenizer")
-
-
