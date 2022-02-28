01d8d09fceddbf381700785c8fbd876c343dce2a
cheng128 <chuchuu128@gmail.com>
2022-02-28 17:26:36 +0800

    [modified] split data into training and validation data

--- data_preprocess/process_MASK_data.py
+++ data_preprocess/process_MASK_data.py
@@ -3,41 +3,49 @@ into training data.
 """
 import json
 import argparse
+from random import sample
 from tqdm import tqdm
 from utils import handle_examples
 
-def brt_groups_results(reserve=False):
-#     with open('../data/cambridge.sense.000.jsonl') as f:
-#         cambridge = [json.loads(line) for line in f.readlines()]
+
+def write_data(filename, data_list):
+    with open(filename, 'w') as f:
+        for data in data_list:
+            f.write('\t'.join(data) + '\n')
     
-#     id2def = {line['id']: line['en_def'] for line in cambridge}
+def brt_groups_results(reserve=False):
     
     with open('../data/remap.word_id.topics.examples.json') as f:
         data = json.loads(f.read())
     
-    with open(f'../data/training_data/reserve_remap_brt_masked_noun.tsv', 'a') as f:
-        for k, v in tqdm(data.items()):
-            if v['pos'] == 'noun':
-                for topic in list(set(v['topics'])):
-                    topic = '['+ topic +']'
-                    headword = v['headword']
-                    for sent_en in v['examples']:
-                        topic_sent, masked_sent = handle_examples(headword, 
-                                                                  sent_en, 
-                                                                  topic, 
-                                                                  reserve)
-                        if topic_sent and masked_sent:
-                            f.write('\t'.join([sent_en, topic_sent, 
-                                               masked_sent]) + '\n')
-#                     definition = headword.capitalize() + ' is ' + id2def[k]
-#                     definition = definition.replace('\n', '')
-#                     topic_sent, masked_sent = handle_examples(headword, 
-#                                                               definition, 
-#                                                               topic, 
-#                                                               reserve)
-#                     if topic_sent and masked_sent:
-#                         f.write('\t'.join([definition, topic_sent, 
-#                                            masked_sent]) + '\n')
+    training_filename = f'../data/training_data/{reserve}_cambridge_train_noun.tsv'
+    print('save training file:', training_filename)
+
+    validation_filename = f'../data/validation_data/{reserve}_cambridge_validate_noun.tsv'
+    print('save validate file:', validation_filename)
+    
+    data_list = []
+    
+    for k, v in tqdm(data.items()):
+        if v['pos'] == 'noun':
+            for topic in list(set(v['topics'])):
+                topic = '['+ topic +']'
+                headword = v['headword']
+                for sent_en in v['examples']:
+                    topic_sent, masked_sent = handle_examples(headword, 
+                                                              sent_en, 
+                                                              topic, 
+                                                              reserve)
+                    if topic_sent and masked_sent:
+                        data_list.append([sent_en, topic_sent, masked_sent])
+                        
+    data_list = sample(data_list, len(data_list))
+    edge = int(len(data_list) * 0.8)
+    training_data = data_list[:edge]
+    validation_data = data_list[edge:]
+    
+    write_data(training_filename, training_data)
+    write_data(validation_filename, validation_data)
 
 def main():
     parser = argparse.ArgumentParser()
