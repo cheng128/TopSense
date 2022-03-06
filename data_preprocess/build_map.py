9579ace803eafa8c043fde6db15862764de1ca89
cheng128 <chuchuu128@gmail.com>
2022-03-06 15:02:28 +0800

    [modified] add version - simple, en

--- data_preprocess/build_map.py
+++ data_preprocess/build_map.py
@@ -1,5 +1,6 @@
 import json
 import nltk
+import argparse
 import wikipediaapi
 import urllib.parse
 from tqdm import tqdm
@@ -7,23 +8,28 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-wiki_wiki = wikipediaapi.Wikipedia(language='en')
-
 
 def load_data():
     with open('../data/words2defs.json') as f:
         word2def = json.loads(f.read())
     
-    return word2def
+    with open('../data/remap.word_id.topics.examples.json') as f:
+        word_id2topics = json.loads(f.read())
+        
+    target_words = set([value['headword'] 
+                        for key, value in word_id2topics.items()
+                        if value['topics']])
+    
+    return word2def, target_words
 
-def build_map(sent, page_def, href_map, word2def):
+def build_map(sent, page_def, href_map, word2def, target_words, wiki_wiki):
     soup = BeautifulSoup(sent, 'lxml')
 
     # deal with each linked word in sentence
     for tag in soup.find_all('a'):
         word = tag.string
         # transform url string into normal text
-        if word and word in word2def:
+        if word and word in word2def and word in target_words: 
             href = urllib.parse.unquote(tag['href'])
             if href not in page_def:
                 is_disambiguate = False
@@ -47,12 +53,28 @@ def build_map(sent, page_def, href_map, word2def):
     return page_def, href_map
 
 def main():
-    word2def = load_data()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', type=str)
+    args = parser.parse_args()
+    
+    version = args.v
+    print(f'{version} wiki')
+    
+    href2def = f'../data/wiki/{version}_wiki_href2def.json'
+    print("href2def:", href2def)
+
+    word2sents = f'../data/wiki/{version}_wiki_href_word2sents.json'
+    print("word2sents:", word2sents)
+
+    wiki_wiki = wikipediaapi.Wikipedia(language=version)
+    
+    word2def, target_words = load_data()
     word_id2wiki_sent = defaultdict(list)
     
     page_def = {}
     href_map = defaultdict(list)
-    with open('../data/wiki/simple_en_wiki_link.txt') as f:
+    with open(f'../data/wiki/{version}_en_wiki_link.txt') as f:
+        count = 0
         for line in tqdm(f.readlines()):
             data = json.loads(line)
             article = data['text']
@@ -60,12 +82,20 @@ def main():
             sentences = tokenizer.tokenize(article)
             
             for sent in sentences:
-                page_def, href_map = build_map(sent, page_def, href_map, word2def)
+                page_def, href_map = build_map(sent, page_def, href_map, 
+                                                word2def, target_words, wiki_wiki)
+            count += 1
+            if count % 10000 == 0:
+                with open(href2def, 'w') as f:
+                    f.write(json.dumps(page_def))
+                    
+                with open(word2sents, 'w') as f:
+                    f.write(json.dumps(href_map)) 
 
-    with open('../data/wiki/wiki_href2def.json', 'w') as f:
+    with open(href2def, 'w') as f:
         f.write(json.dumps(page_def))
         
-    with open('../data/wiki/wiki_href_word2sents.json', 'w') as f:
+    with open(word2sents, 'w') as f:
         f.write(json.dumps(href_map))
 
 if __name__ == '__main__':
