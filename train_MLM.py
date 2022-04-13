5c8598e1569454ba0183291b75295ef7b3dd8b08
cheng128 <chuchuu128@gmail.com>
2022-04-13 10:20:51 +0800

    [modified] tokenizer, args, lr

--- train_MLM.py
+++ train_MLM.py
@@ -17,15 +17,6 @@ class TopicDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.encodings.input_ids)
 
-# def save_tokenizer():
-#     with open('./data/cambridge_topic.000.jsonl') as f:
-#         cam2top = [json.loads(line) for line in f.readlines()]
-
-#     new_tokens = ['['+i['topic'][0]+']' for i in cam2top if i['pos']=='noun']
-#     num_added_toks = tokenizer.add_tokens(new_tokens)
-#     tokenizer.save_pretrained('./tokenizer')
-#     print('after save vocabulary')
-
 def load_preprocessed(filename):
     topic_sent = []
     masked_sent = []
@@ -44,21 +35,23 @@ def main():
     parser.add_argument('-f', type=str) # preprocess file name
     parser.add_argument('-g', type=str) # directoy
     parser.add_argument('--save', action="store_true")
-    parser.add_argument('-n', type=str)
+    parser.add_argument('-n', type=str, default='all')
     parser.add_argument('-r', type=int, default=0)
-    parser.add_argument('-v', type=str)
+    parser.add_argument('-lr', type=float, default=1e-5)
     args = parser.parse_args()
     
     epochs = args.e
     filename = args.f
     reserve = bool(args.r)
-    version = args.v
+    lr_rate = args.lr
+    print('lr_rate:', lr_rate)
 
-    map_dict = {'brt': 'brt', 'wiki': 'wikipedia', 'concat': 'concat', 'hybrid': 'hybrid'}
+    map_dict = {'brt': 'brt', 'wiki': 'wikipedia', 'concat': 'concat', 'hybrid': 'hybrid',
+                'remap/cam': 'remap/cam', 'remap/concat': 'remap/concat', 'remap/hybrid': 'remap/hybrid',
+                'btwn': 'btwn'}
     
     if args.save:
-        dir_path = f'./model/{map_dict[args.g]}/highest_{args.n}_{version}_{reserve}_{epochs}epochs'
-        # dir_path = f'./model/{map_dict[args.g]}/{args.g}_{reserve}_{args.e}epochs_base_cam_False_10'
+        dir_path = f'./model/{map_dict[args.g]}/{args.n}_{reserve}_{epochs}epochs'
         try:
             os.mkdir(dir_path)
         except:
@@ -66,26 +59,20 @@ def main():
             os.mkdir(dir_path)
         print('model will be saved in: ', dir_path)
         
-    tokenizer_name = 'remap_tokenizer'
+    tokenizer_name = 'tokenizer_casedFalse'
         
     tokenizer = BertTokenizerFast.from_pretrained(tokenizer_name)
     print("use tokenizer:", tokenizer_name)
     
     base_pretrained_model = 'bert-base-uncased'
-#     base_pretrained_model =  './model/concat/highest_20_simple_False_10epochs'
     model = BertForMaskedLM.from_pretrained(base_pretrained_model)
-    
-#     from wiki base pretrained model
-#     pretrain = './model/wikipedia/20-t5-xl_remap_10epochs_base0'
-#     print('from pretrain: ', pretrain)
-#     model = BertForMaskedLM.from_pretrained(pretrain)
+
     # must implement
     model.resize_token_embeddings(len(tokenizer))
     
     topic_sent, masked_sent = load_preprocessed(filename)
-    print('after load data')
     
-    device = torch.device('cuda:1') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     print('device: ', device)
 
     inputs = tokenizer(masked_sent,
@@ -107,7 +94,7 @@ def main():
     model.to(device)
     model.train()
 
-    optim = AdamW(model.parameters(), lr=1e-5)
+    optim = AdamW(model.parameters(), lr=lr_rate)
     loss_record = []
     for epoch in range(epochs):
         loop = tqdm(dataloader, leave=True)
@@ -128,7 +115,7 @@ def main():
     if args.save:
         model.save_pretrained(dir_path)
         with open(f'{dir_path}/spec.txt', 'w') as f:
-            f.write(f'python train_MLM.py -e {epochs} -g {args.g} -f {filename} -n {args.n} -r {args.r} -v {version}\n')
+            f.write(f'python train_MLM.py -e {epochs} -g {args.g} -f {filename} -n {args.n} -r {args.r}\n')
             f.write(f'tokenizer: {tokenizer_name}\n')
             f.write(f'pretrained model: {base_pretrained_model}\n')
             for loss in loss_record:
